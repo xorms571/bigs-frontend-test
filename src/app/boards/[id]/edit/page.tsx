18e65ac9b0189474bc1e7b0b241ae220eefee59a
xorms571 <xorms6865@naver.com>
2025-10-31 09:03:47 +0900

    글 수정 페이지 params 타입 수정

--- src/app/boards/[id]/edit/page.tsx
+++ src/app/boards/[id]/edit/page.tsx
@@ -3,13 +3,14 @@ import Container from '@/components/Container';
 import Title from '@/components/Title';
 import { Suspense } from 'react';
 
-export default function EditBoardPage({ params }: { params: { id: string } }) {
+export default async function EditBoardPage({ params }: { params: Promise<{ id: string }> }) {
+  const { id } = await params;
   return (
-    <Suspense fallback={<div>Loading Edit Board Page...</div>}>
+    <Suspense fallback={<div>Loading New Board Page...</div>}>
       <Container>
         <Title className="mb-2">글 수정</Title>
-        <BoardForm isEditMode boardId={params.id} />
+        <BoardForm isEditMode boardId={id} />
       </Container>
     </Suspense>
-  );
+  )
 }
